9e5cd972030fd4307e2e4f3d299e0e3101ee4499
Meysam <mey.abd370@gmail.com>
2020-01-23 14:13:12 +0330

    Add Auto Map ID Code

--- sfg-pet-data/src/main/java/mey/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ sfg-pet-data/src/main/java/mey/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -25,30 +25,25 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("mey");
         owner1.setLastName("abd");
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("sae");
         owner2.setLastName("bab");
         ownerService.save(owner2);
 
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("ali");
         vet1.setLastName("ahm");
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
-        vet2.setFirstName("ali");
-        vet2.setLastName("ahm");
+        vet2.setFirstName("sam");
+        vet2.setLastName("shi");
         vetService.save(vet2);
-
 
     }
 
